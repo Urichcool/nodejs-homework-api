3baf14e4d972efc804aa4527d51117c0193e47ed
Yurii Novikov <104870932+Urichcool@users.noreply.github.com>
2023-03-09 22:28:05 +0100

    update-users-service

--- service/contacts.js
+++ service/contacts.js
@@ -1,21 +1,35 @@
-const Contact = require('./schemas/contacts')
+const Contact = require("./schemas/contacts");
 
-const getAllContacts = async () => Contact.find()
+const getAllContacts = async () => Contact.find();
 
-const getContactById = async contactId => Contact.findById(contactId)
+const getContactsByQbe = async (qbe) => Contact.find(qbe);
+
+const getContactById = async (contactId) => Contact.findById(contactId);
 
 const createContact = async ({ name, email, phone, favorite }) => {
-	return Contact.create({ name, email, phone, favorite })
-}
+  return Contact.create({ name, email, phone, favorite });
+};
 
 const updateContact = async (contactId, fields) => {
-	return Contact.findByIdAndUpdate(contactId, fields, { new: true, strict: 'throw', runValidators: true })
-}
+  return Contact.findByIdAndUpdate(contactId, fields, {
+    new: true,
+    strict: "throw",
+    runValidators: true,
+  });
+};
 
 const updateStatusContact = async (contactId, favorite) => {
-	return Contact.findByIdAndUpdate(contactId, { favorite })
-}
+  return Contact.findByIdAndUpdate(contactId, { favorite });
+};
 
-const deleteContact = async contactId => Contact.findByIdAndRemove(contactId)
+const deleteContact = async (contactId) => Contact.findByIdAndRemove(contactId);
 
-module.exports = { getAllContacts, getContactById, createContact, updateContact, updateStatusContact, deleteContact }
+module.exports = {
+  getAllContacts,
+  getContactsByQbe,
+  getContactById,
+  createContact,
+  updateContact,
+  updateStatusContact,
+  deleteContact,
+};
--- service/users.js
+++ service/users.js
@@ -1,5 +1,21 @@
-const User = require('./schemas/user')
+const User = require("./schemas/user");
 
-const getUser = async (body) => User.findOne(body)
+const getUser = async (body) => User.findOne(body);
 
-module.exports = { getUser }
+const updateUserSubscription = async (userId, subscription) => {
+  return User.findByIdAndUpdate(userId, { subscription });
+};
+
+const updateUserAvatar = async (userId, avatarUrl) => {
+  return User.findByIdAndUpdate(userId, { avatarUrl });
+};
+
+const deleteUser = async (userMail) =>
+  User.findOneAndDelete({ email: userMail });
+
+module.exports = {
+  getUser,
+  updateUserSubscription,
+  updateUserAvatar,
+  deleteUser,
+};
